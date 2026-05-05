ef5c9bfa23d1e31ff19a6687bab5c91e2abbcd57
SRIRAM VELUMURI <86483840+Infinity-tracer@users.noreply.github.com>
2026-05-06 00:41:51 +0200

    first commit

---
+++ RoadDamageDetection-main/Home.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Road Damage Detections Apps",
+    page_icon="🛣️",
+)
+
+st.divider()
+st.title("Road Damage Detection Application")
+
+st.divider()
+
